b2e8ec6b0c8ca2c7d24e0dde944c8c83788335fe
Daniel Yuschick <yuschick@users.noreply.github.com>
2022-12-02 23:11:27 +0200

    Basic Setup of ESLint and Prettier (#1)

--- packages/moaxe-ui/src/components/layout/Box/Box.tsx
+++ packages/moaxe-ui/src/components/layout/Box/Box.tsx
@@ -1,8 +1,8 @@
-import { PropsWithChildren } from "react";
-import { WithTestId } from "../../../types";
+import { PropsWithChildren } from 'react';
+import { WithTestId } from '../../../types';
 
 export interface BoxProps extends PropsWithChildren, WithTestId {}
 
 export function Box({ children }: BoxProps) {
-  return <div>{children}</div>;
+    return <div>{children}</div>;
 }
